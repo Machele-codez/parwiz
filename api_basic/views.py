86c303d1c2ddccf3d519074e4cd8ce5dcbed29ad
* <smithbeblack@gmail.com>
2020-07-26 22:36:06 +0000

    done

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse 
-from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Article
 from .serializers import ArticleModelSerializer
@@ -9,7 +12,7 @@ from .serializers import ArticleModelSerializer
 from pprint import pprint
 # Create your views here.
 
-@csrf_exempt # this allows posts to this view without a csrf token
+@api_view(['GET', 'POST'])# this enables full rest_framework view functionality (web browsable api)
 def article_list_view(request):
   
   if request.method == 'GET':
@@ -18,48 +21,48 @@ def article_list_view(request):
     """
     articles = Article.objects.all()
     serializer = ArticleModelSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
     
   elif request.method == 'POST':
     """
     adding an article
     """
-    data = JSONParser().parse(request) #parse the request into a python dict
+    data = request.data #already parsed
     pprint(data)
     serializer = ArticleModelSerializer(data=data)
     
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data, status=201)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    return JsonResponse(serializer.errors, status=400) 
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
     
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
   try:
     article = Article.objects.get(pk=pk)
   except Article.DoesNotExist:
-    return HttpResponse("Requested Article Does Not Exist", status=404)
+    return HttpResponse("Requested Article Does Not Exist", status=status.HTTP_404_NOT_FOUND)
     
   if request.method == 'GET':
     """
     article detail
     """
     serializer = ArticleModelSerializer(article)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
     
   elif request.method == 'PUT':
     """
     update an article
     """
-    data = JSONParser().parse(request)
+    data = request.data
     serializer = ArticleModelSerializer(article, data=data)
     
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
   elif request.method == 'DELETE':
     """
@@ -67,4 +70,4 @@ def article_detail(request, pk):
     """
     article_title = article.title
     article.delete()
-    return HttpResponse(f"{article_title} deleted!", status=204)
+    return Response(f"{article_title} deleted!", status=status.HTTP_204_NO_CONTENT)
